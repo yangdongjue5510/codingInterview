122846806b31fa5e80033a6ba7212e47a03f5a5d
yangdongjue5510 <djwhy5510@gmail.com>
2021-11-14 21:03:02 +0900

    No11501

---
+++ Codes/src/week9/No11501.java
@@ -0,0 +1,25 @@
+package week9;
+import java.io.*;
+import java.util.*;
+public class No11501 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(bf.readLine());
+        for(int j = 0 ; j < testCase ; j++){
+            int dayCount = Integer.parseInt(bf.readLine());
+            long[] days = new long[dayCount];
+            StringTokenizer st = new StringTokenizer(bf.readLine());
+            for(int i = 0 ; i  < dayCount ; i++){
+                long tmp = Long.parseLong(st.nextToken());
+                days[i] = tmp;
+            }
+            long max = 0;
+            long result = 0;
+            for(int i = dayCount-1 ; i >= 0 ; i--){
+                if(max < days[i]) max = days[i];
+                else result+=(max-days[i]);
+            }
+            System.out.println(result);
+        }
+    }
+}
